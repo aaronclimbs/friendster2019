1568fb93dbb17281685e7f91649378ee966b70f8
Aaron Keisler <amkeisler@gmail.com>
2019-08-09 08:27:17 -0400

    add stock photo links

--- backup.sql
+++ backup.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `friends`;
 CREATE TABLE `friends` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `user_pic` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_pic` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `answers` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -37,7 +37,7 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
-INSERT INTO `friends` VALUES (1,'Noah','photo.jpg','1,4,3,5,2,3,4,4,1,1'),(2,'Ava','photo.jpg','1,4,1,1,3,4,4,1,3,1'),(3,'Liam','photo.jpg','5,1,2,2,1,4,1,5,1,1'),(4,'Isabella','photo.jpg','1,5,3,4,2,5,4,5,2,4'),(5,'Jacob','photo.jpg','1,2,5,5,3,2,2,1,4,5'),(6,'Mia','photo.jpg','3,5,2,2,3,2,5,2,3,5'),(7,'Mason','photo.jpg','5,2,5,1,1,1,3,4,5,2'),(8,'Abigail','photo.jpg','4,2,3,3,4,3,1,3,5,2'),(9,'William','photo.jpg','5,3,2,1,4,5,3,3,4,2'),(10,'Emily','photo.jpg','3,2,3,4,1,3,1,5,3,5'),(11,'Ethan','photo.jpg','3,5,3,1,5,4,3,3,5,2'),(12,'Charlotte','photo.jpg','3,3,3,4,3,3,4,2,4,3'),(13,'Noah','http:.jpg','5,1,4,3,1,4,3,1,3,3');
+INSERT INTO `friends` VALUES (1,'Noah','https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','1,4,3,5,2,3,4,4,1,1'),(2,'Ava','https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','1,4,1,1,3,4,4,1,3,1'),(3,'Liam','https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','5,1,2,2,1,4,1,5,1,1'),(4,'Isabella','https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','1,5,3,4,2,5,4,5,2,4'),(5,'Jacob','https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','1,2,5,5,3,2,2,1,4,5'),(6,'Mia','https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','3,5,2,2,3,2,5,2,3,5'),(7,'Mason','https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','5,2,5,1,1,1,3,4,5,2'),(8,'Abigail','https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','4,2,3,3,4,3,1,3,5,2'),(9,'William','https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','5,3,2,1,4,5,3,3,4,2'),(10,'Emily','https://images.pexels.com/photos/638700/pexels-photo-638700.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','3,2,3,4,1,3,1,5,3,5'),(11,'Ethan','https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','3,5,3,1,5,4,3,3,5,2'),(12,'Charlotte','https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','3,3,3,4,3,3,4,2,4,3'),(13,'Noah','https://images.pexels.com/photos/462680/pexels-photo-462680.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','5,1,4,3,1,4,3,1,3,3');
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
